f04918a91c2f61f052de56f1ec63eb5376881c4e
Simon Lewis <lewissimonjohn@gmail.com>
2021-07-16 16:31:03 -0700

    Revert "Hope this doesnt screw anything up"
    This reverts commit 78a1db81e7d71de73dcab8033970e94838f09a05.

--- MidtermProject/main.cpp
+++ MidtermProject/main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 using std::string;
 
 
+
+
 int main()
 {
 	cout << "Welcome to the Imaginary College" << endl;
@@ -25,11 +27,10 @@ int main()
 	Date personEndDate;
 	cout << "Please enter the end date of semester is this format: 7/4/2021 \n";
 	cin >> personEndDate;
-	int numOfClasses;
-	cout << "How many classes do you plan to take?" << endl;
-	cin >> numOfClasses;
 
-	while (cout << "How many classes do you plan to take? " && !(cin >> numOfClasses))
+	int numOfClasses;
+
+	while (cout << "How many classes do you plan to take?" && !(cin >> numOfClasses))
 	{
 		cin.clear();
 		cin.ignore(numeric_limits < streamsize>::max(), '\n');
@@ -49,8 +50,7 @@ int main()
 		cout << "1) Enter a new course\n";
 		cout << "2) Remove a course\n";
 		cout << "3) Print a Semester Schedule\n";
-		cout << "q) Quit the program" << endl
-			<< "Enter your choice: ";
+		cout << "q) Quit the program" << endl;
 
 		cin >> choice;
 
@@ -59,46 +59,78 @@ int main()
 			string classNumber;
 			string className;
 			string classDays;
-			int unitsValue;
+			double unitsValue;
 
-			// Asking for class number, class name, class meeting days and class unit
-			cout << "\nEntering a new course selected\n";
-			cout << "----------------------------------\n";
-			cout << "Please provide a course number: ";
-			getline(cin, classNumber);
-			cout << "Please provide a course name: ";
-			getline(cin, className);
-			cout << "Please provide the meeting days for the course: ";
-			getline(cin, classDays);
-			cout << "Please provide the number of units the course is worth: ";
-			cin >> unitsValue;
+			if (personCourseSchedule.getNumCourses() != personCourseSchedule.getMaxSize())
+			{
+				// Asking for class number, class name, class meeting days and class unit
+				cout << "\nEntering a new course selected\n";
+				cout << "----------------------------------" << endl;
+				cin.ignore();
+				cout << "Please provide a course number: ";
+				getline(cin, classNumber);
 
-			// Asking for start and end time
-			Time classStartTime;
-			cout << "Please enter the starting time for the course in this format:   7:30 PM\n";
-			cin >> classStartTime;
-			Time classEndTime;
-			cout << "Please enter the ending time for the course in this format:   7:30 PM\n";
-			cin >> classEndTime;
+				cout << "Please provide a course name: ";
+				getline(cin, className);
 
-			// Asking for start and end date
-			Date classStartDate;
-			cout << "Please enter the start date for the course in this format: 7/4/2021 \n";
-			do {
-			cin >> classStartDate;
-			} while ()
-			Date classEndDate;
-			cout << "Please enter the end date for the course in this format: 7/4/2021 \n";
-			cin >> classEndDate;
+				cout << "Please provide the meeting days for the course: ";
+				getline(cin, classDays);
 
-			Course course1(className, classNumber, classDays, unitsValue, classStartDate, classEndDate, classStartTime, classEndTime);
+				while (cout << "Please provide the number of units the course is worth: " && !(cin >> unitsValue))
+				{
+					cin.clear();
+					cin.ignore(numeric_limits < streamsize>::max(), '\n');
+					cout << "\nInvalid input. Please re-enter a number.\n" << endl;
+				}
 
-			// Adding course1 to user course schedule
-			personCourseSchedule.addCourse(course1);
+				// Asking for start and end time
+				Time classStartTime;
+				cout << "Please enter the starting time for the course in this format:   7:30 PM\n";
+				cin >> classStartTime;
+				Time classEndTime;
+				cout << "Please enter the ending time for the course in this format:   7:30 PM\n";
+				cin >> classEndTime;
+
+				// Asking for start and end date
+				Date classStartDate;
+				cout << "Please enter the start date for the course in this format: 7/4/2021 \n";
+				cin >> classStartDate;
+				Date classEndDate;
+				cout << "Please enter the end date for the course in this format: 7/4/2021 \n";
+				cin >> classEndDate;
+
+				Course courseToAdd(className, classNumber, classDays, unitsValue, classStartDate, classEndDate, classStartTime, classEndTime);
+
+				// Adding course1 to user course schedule
+				personCourseSchedule.addCourse(courseToAdd);
+
+			}
+			else
+			{
+				cout << "Your plate is full. You can not add another class to the course schedule\n" << endl;
+			}
+
+			
 		}
 		
 		if (choice == '2')
 		{
+			if (personCourseSchedule.getNumCourses() != 0)
+			{
+				string removeClass;
+				cout << "Please type in the course number you would like to remove from the course schedule: ";
+				cin.ignore();
+				getline(cin, removeClass);
+
+				personCourseSchedule.removeCourse(removeClass);
+
+				cout << endl;
+				Course courseToRemove;
+			}
+			else
+			{
+				cout << "You have no class to remove.\n" << endl;
+			}
 
 		}
 
@@ -107,17 +139,17 @@ int main()
 			cout << personCourseSchedule;
 		}
 
+		if (choice != '1' && choice != '2' && choice != '3' && choice != 'q' && choice != 'Q')
+		{
+			cout << "Invalid selection!\n" << endl;
+		}
+
 	} while (choice != 'q' && choice != 'Q');
 
 	// Need print out of invalid menu selection
 
 	return 0;
 }
-
-
-
-
-
 
 
 
